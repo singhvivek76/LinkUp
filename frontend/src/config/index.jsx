b3b4e9006a36c37e0f853ea21f66de00c35abff5
vivek-singh76 <vivek.kumarsingh.cs.@2023mitmeerut.ac.in>
2026-04-16 23:29:16 +0530

    Update frontend

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,7 @@
 const { default: axios } = require("axios");
 
-export const BASE_URL = "http://localhost:9080"
+export const BASE_URL = "http://localhost:9080" || process.env.NEXT_PUBLIC_API_URL || "/_/backend"
+
 
 export const clientServer = axios.create({
     baseURL: BASE_URL,
