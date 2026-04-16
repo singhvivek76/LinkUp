c1be6af27175ac959a212e01a1b275463ca84177
vivek-singh76 <vivek.kumarsingh.cs.@2023mitmeerut.ac.in>
2026-04-16 23:33:27 +0530

    Update frontend

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -60,7 +60,7 @@ const handleLogin = () => {
           <div className={styles.cardContainer_left}>
             <p className={styles.cardleft_heading}> {userLoginMethod ? "Sign In" : "Sign Up"} </p>
 
-            <p style={{color: authState.isError ? "red" : "green"}}> {" "} {authState.message.message} </p>
+            <p style={{color: authState.isError ? "red" : "green"}}> {authState.message.message} </p>
 
               <div className={styles.inputContainers}>
 
